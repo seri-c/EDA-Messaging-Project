6178c627476d098e80fd5a538fbf2cdee42b60d4
Christelle <christouseri@gmail.com>
2026-07-01 19:15:19 -0500

    Adding new methods to GlobalExceptionHandler to handle NoResourceFound and bind exceptions

--- notification-system/message-service/src/main/java/com/project/messageservice/exception/GlobalExceptionHandler.java
+++ notification-system/message-service/src/main/java/com/project/messageservice/exception/GlobalExceptionHandler.java
@@ -5,92 +5,127 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.bind.BindException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+        private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(MessageNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleMessageNotFound(
-            MessageNotFoundException ex,
-            HttpServletRequest request) {
+        @ExceptionHandler(MessageNotFoundException.class)
+        public ResponseEntity<ErrorResponse> handleMessageNotFound(
+                        MessageNotFoundException ex,
+                        HttpServletRequest request) {
 
-        ErrorResponse error = new ErrorResponse(
-                Instant.now(),
-                HttpStatus.NOT_FOUND.value(),
-                HttpStatus.NOT_FOUND.getReasonPhrase(),
-                ex.getMessage(),
-                request.getRequestURI(),
-                null);
+                ErrorResponse error = new ErrorResponse(
+                                Instant.now(),
+                                HttpStatus.NOT_FOUND.value(),
+                                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                                ex.getMessage(),
+                                request.getRequestURI(),
+                                null);
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(error);
-    }
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                .body(error);
+        }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex,
-            HttpServletRequest request) {
-        ErrorResponse error = new ErrorResponse(
-                Instant.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                ex.getMessage(),
-                request.getRequestURI(),
-                null);
+        @ExceptionHandler(IllegalArgumentException.class)
+        public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex,
+                        HttpServletRequest request) {
+                ErrorResponse error = new ErrorResponse(
+                                Instant.now(),
+                                HttpStatus.BAD_REQUEST.value(),
+                                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                                ex.getMessage(),
+                                request.getRequestURI(),
+                                null);
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(error);
-    }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                .body(error);
+        }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationException(
-            MethodArgumentNotValidException ex,
-            HttpServletRequest request) {
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<ErrorResponse> handleValidationException(
+                        MethodArgumentNotValidException ex,
+                        HttpServletRequest request) {
 
-        List<FieldErrorResponse> fieldErrors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(error -> new FieldErrorResponse(
-                        error.getField(),
-                        error.getDefaultMessage()))
-                .toList();
+                List<FieldErrorResponse> fieldErrors = ex.getBindingResult()
+                                .getFieldErrors()
+                                .stream()
+                                .map(error -> new FieldErrorResponse(
+                                                error.getField(),
+                                                error.getDefaultMessage()))
+                                .toList();
 
-        ErrorResponse response = new ErrorResponse(
-                Instant.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                "Validation failed",
-                request.getRequestURI(),
-                fieldErrors);
+                ErrorResponse response = new ErrorResponse(
+                                Instant.now(),
+                                HttpStatus.BAD_REQUEST.value(),
+                                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                                "Validation failed",
+                                request.getRequestURI(),
+                                fieldErrors);
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleUnexpected(
-            Exception ex,
-            HttpServletRequest request) {
+        @ExceptionHandler(NoResourceFoundException.class)
+        public ResponseEntity<ErrorResponse> handleNotFound(NoResourceFoundException ex,
+                        HttpServletRequest request) {
 
-        log.error("Unexpected exception", ex);
+                ErrorResponse response = new ErrorResponse(
+                                Instant.now(),
+                                HttpStatus.BAD_REQUEST.value(),
+                                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                                "Path not found",
+                                request.getRequestURI(),
+                                null);
 
-        ErrorResponse error = new ErrorResponse(
-                Instant.now(),
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
-                "An unexpected error occured",
-                request.getRequestURI(),
-                null);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(error);
+        @ExceptionHandler({ MethodArgumentTypeMismatchException.class,
+                        MissingServletRequestParameterException.class,
+                        BindException.class })
+        public ResponseEntity<ErrorResponse> handleBadRequest(Exception ex, HttpServletRequest request) {
 
-    }
+                  ErrorResponse response = new ErrorResponse(
+                                Instant.now(),
+                                HttpStatus.BAD_REQUEST.value(),
+                                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                                "Unable to parse parameters",
+                                request.getRequestURI(),
+                                null);
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        @ExceptionHandler(Exception.class)
+        public ResponseEntity<ErrorResponse> handleUnexpected(
+                        Exception ex,
+                        HttpServletRequest request) {
+
+                log.error("Unexpected exception", ex);
+
+                ErrorResponse error = new ErrorResponse(
+                                Instant.now(),
+                                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                                "An unexpected error occured",
+                                request.getRequestURI(),
+                                null);
+
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                .body(error);
+
+        }
 }
